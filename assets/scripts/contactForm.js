b4bc65add05eb79463f6763576d772b6cdf533c5
Eleana Tyradelli <el.tyradelli@gmail.com>
2024-06-05 00:44:48 +0300

    Added submit messages in form

--- assets/scripts/contactForm.js
+++ assets/scripts/contactForm.js
@@ -1,6 +1,9 @@
 "use strict";
 const formFields = document.querySelectorAll(".form-user-input");
 const submitBtn = document.getElementById("submit");
+const missingMsg = document.getElementById("form-submit-missing-message");
+const thankYouMsg = document.getElementById("form-submit-success-message");
+const sentErrorMsg = document.getElementById("form-submit-error-message");
 
 formFields.forEach((field) => {
   field.addEventListener("focusin", function () {
@@ -17,11 +20,13 @@ formFields.forEach((field) => {
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   e.stopPropagation();
+  toggleActive(missingMsg, "active", true);
 
   let formCompleted = true;
 
   for (let field of formFields) {
     if (!field.value.trim().length) {
+      toggleActive(missingMsg);
       console.log("Please fill in all form fields");
       formCompleted = false;
       break;
@@ -36,10 +41,25 @@ submitBtn.addEventListener("click", function (e) {
 function sendMail() {
   emailjs.sendForm("default_service", "template_space", "#contact-form").then(
     function (res) {
-      console.log("Success!" + res.status);
+      resetForm();
+      toggleActive(thankYouMsg);
+      setTimeout(() => {
+        toggleActive(thankYouMsg);
+      }, 5000);
     },
-    (error) => {
-      console.log("FAILED...", error);
+    function (error) {
+      resetForm();
+      toggleActive(sentErrorMsg);
+      setTimeout(() => {
+        toggleActive(sentErrorMsg);
+      }, 5000);
     }
   );
+}
+
+function resetForm() {
+  formFields.forEach((field) => {
+    field.value = "";
+    field.nextElementSibling.classList.remove("focused-label");
+  });
 }
